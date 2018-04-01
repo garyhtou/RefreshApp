d4e06aa1f0f7789fe092403c265cf69621606fbe
mschwamborn <mikems@live.com>
2018-04-01 16:28:35 -0700

    Work on Meal screen and Account screen. Need to do Notifications, Queue and meal activities

--- app/src/main/java/resfreshapp/refresh/MainActivity.java
+++ app/src/main/java/resfreshapp/refresh/MainActivity.java
@@ -3,9 +3,15 @@ package resfreshapp.refresh;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +22,17 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_meals:
+                    transaction.replace(R.id.content,new MealFragment()).commit();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_basket:
+                    transaction.replace(R.id.content,new BasketFragment()).commit();
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_account:
+                    transaction.replace(R.id.content,new AccountFragment()).commit();
                     return true;
             }
             return false;
@@ -36,9 +44,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        Toolbar Toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(Toolbar);
+        getSupportActionBar().setTitle("");
+        ((Toolbar) findViewById(R.id.toolbar)).setLogo(R.drawable.refresh_name_white);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content,new MealFragment()).commit();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.top_bar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.action_notification:/*
+                Intent intent1 =  new Intent(Class1Homework.this, addHomework1.class);
+                startActivity(intent1);*/
+                Toast t = Toast.makeText(getApplicationContext(), "Notifications", Toast.LENGTH_LONG);
+                t.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
